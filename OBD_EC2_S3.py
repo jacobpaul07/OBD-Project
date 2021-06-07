ac2366a95eec6f552583b10bd7add572334c87ec
Jacob Paul <46052000+jacobpaul07@users.noreply.github.com>
2021-06-07 17:03:14 +0530

    Update OBD_EC2_S3.py

--- OBD_EC2_S3.py
+++ OBD_EC2_S3.py
@@ -53,15 +53,14 @@ if __name__ == '__main__':
                     data = conn.recv(1024)
                     print("TimeStamp: ", datetime.datetime.now())
                     print(data)
-                    byte_data = data.decode("utf-8")
-                    list = byte_data.split(",")
+                    raw_data = data.decode("utf-8")
+                    list = raw_data.split(",")
                     #print(list)
                     if not data:
                         break
                     # --------- This is the input to Server every minute ---------
                     # raw_data = 'L,ATLOBD,866795030623415,03,6816,073203,070419,CAN,0101:00076100,0103:0200,0104:69,0105:7F,010A:XX,010B:4D,010C:1977,010D:2F,010E:BC,010F:54,0110:XXXX,0111:2B,011C:06,011F:012D,0121:0000,0122:XXXX,012F:XX,0162:XX,0132:XXXX,0133:60,0143:006E,0145:13,0146:XX,0147:2E,0148:XX,0149:2F,014A:17,014B:XX,014C:1F,0151:XX,0131:9174,0144:XXXX,015E:XXXX,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,0902:XXXXXXXXXXXXXXXXXXXX,$ATLOBD,*8'
                     #byte_data = b'L,ATL,866039048589957,02,7510,070339,070621,A,17.488947;N,78.346046;E,0,251,19.84,4.2,15,404,49,4f34,6754,#0031,0,ATL,*k'
-                    raw_data = data.decode('UTF-8')
 
                     # --------- Call function here ---------
                     print("\t----- Result Output To Be Stored In Database -----")
@@ -69,6 +68,7 @@ if __name__ == '__main__':
                     print(information)
 
                     if information["Live/Memory"] == "L":
+
                         cli.put_object(
                             Body=str(information),
                             Bucket='ec2-obd2-bucket',
@@ -79,8 +79,10 @@ if __name__ == '__main__':
                             Body=str(information),
                             Bucket='ec2-obd2-bucket',
                             Key='H/OBD2--{}.txt'.format(str(datetime.datetime.now())))
+
                     # Send response to OBD
                     a = b'@866039048589957,00,1234,*CS'
                     conn.send(b'@866039048589957,00,7318,*CS')
                     print("--------------------------------------------------------------------------------------------------------")
+
 
