d8a05e717b3e2189747b6b63144586756e14ca76
Jacob Paul <46052000+jacobpaul07@users.noreply.github.com>
2021-06-18 01:31:07 +0530

    Add files via upload

---
+++ GPS-OBD2/obd_gps.py
@@ -0,0 +1,76 @@
+import folium
+import numpy as np
+from IPython.display import HTML
+from math import sin, cos, sqrt, atan2, radians
+
+radius = 500
+
+
+def gps_one(lat, lon):
+    mapit = folium.Map(location=[lat, lon], zoom_start=15)
+    folium.Marker(location=[lat, lon],
+                  popup='OBD Initial Location (ID): <br> 65615765277',
+                  fill_color='#43d9de',
+                  tooltip="Initial location",
+                  radius=radius).add_to(mapit)
+    folium.Circle([lat, lon],
+                  radius=radius,
+                  fill=True,
+                  ).add_to(mapit)
+    mapit.save('./GeoMaps/current_0_location.html')
+    HTML('<iframe src=./GeoMaps/current_0_location.html width=700 height=450></ifrme>')
+
+
+def gps_main(lat, lon, lat_live, lon_live):
+    # approximate radius of earth in km
+    R = 6373.0
+    # ------------------------------------------------
+    lat1 = radians(float(lat))
+    lon1 = radians(float(lon))
+    # ------------------------------------------------
+    print("Initial Device Latitude : ", lat)
+    print("Initial Device Longitude: ", lon)
+
+    lat2 = radians(float(lat_live))
+    lon2 = radians(float(lon_live))
+    # ------------------------------------------------
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    print("Result:")
+    print("In KM.   : ", distance, "km")
+    print("In meters: ", distance * 1000, "m")
+
+    if distance * 1000 <= float(radius):
+        print("OBD device is under given area")
+    else:
+        print("OBD device is NOT under given area")
+
+    map_obd = folium.Map(location=[lat, lon], zoom_start=15)
+    folium.Marker(location=[lat, lon], fill_color='#43d9de', radius=radius).add_to(map_obd)
+    if distance * 1000 <= float(radius):
+        folium.Marker(location=[lat_live, lon_live],
+                      popup='OBD Device No: <br> 65615765277',
+                      # icon=folium.Icon(icon="cloud", color='green'),
+                      tooltip="OBD Device",
+                      radius=radius).add_to(map_obd)
+    else:
+        folium.Marker(location=[lat_live, lon_live],
+                      popup='OBD Device No: <br> 65615765277',
+                      # icon=folium.Icon(icon="cloud", color='red'),
+                      tooltip="OBD Device",
+                      radius=radius).add_to(map_obd)
+
+    folium.Circle([lat, lon],
+                  radius=radius,
+                  fill=True,
+                  ).add_to(map_obd)
+    map_obd.add_child(folium.LatLngPopup())
+    map_obd.save('./GeoMaps/current_OBD_current_location.html')
+    HTML('<iframe src=./GeoMaps/current_OBD_current_location.html width=700 height=450></ifrme>')
---
+++ GPS-OBD2/obd_gps_formatted.py
@@ -0,0 +1,169 @@
+import socket
+import datetime
+import pandas as pd
+import numpy as np
+import re
+from obd_gps import gps_one, gps_main
+
+
+def convert_LOGIN_data(login_data):
+    """
+    Function that'll convert Raw LOGIN data to Readable JSON Object
+    """
+    # --------- Headers for final dictionary ---------
+    HEADERS = ['Live/Memory', 'Signature', 'IMEI', 'Message Type', 'Sequence No', 'CHECKSUM']
+
+    # Result dictionary
+    result = {}
+
+    # --------- Data Processing ---------
+    for index, header in enumerate(HEADERS):
+        result[header] = login_data[index]
+
+    return result
+
+
+def convert_GPS_data(gps_data):
+    """
+    Function that'll convert Raw GPS data to Readable JSON Object
+    """
+
+    if len(gps_data) == 23:
+        # --------- Headers for final dictionary ---------
+        HEADERS = ['Live/Memory', 'Signature', 'IMEI', 'Message Type', 'Sequence No', 'Time (GMT)', 'Date',
+                   'valid/invalid', 'Latitude', 'Longitude', 'Speed (knots)', 'Angle of motion', 'Odometer (KM)',
+                   'Internal battery Level (Volts)', 'Signal Strength', 'Mobile country code', 'Mobile network code',
+                   'Cell id', 'Location area code',
+                   '#Ignition(0/1), RESERVED ,Harsh Braking / Acceleration//Non(0/2/3),Main power status(0/1)',
+                   'Over speeding', 'Signature', 'CHECKSUM']
+
+        # Result dictionary
+        result = {}
+
+        # --------- Data Processing ---------
+        for index, header in enumerate(HEADERS):
+            result[header] = gps_data[index]
+
+        return result
+
+    else:
+        # --------- Headers for final dictionary ---------
+        HEADERS = ['Live/Memory', 'Signature', 'IMEI', 'Message Type', 'Sequence No', 'Time (GMT)', 'Date',
+                   'valid/invalid', 'Latitude', 'North/South', 'Longitude', 'East/West', 'Speed (knots)',
+                   'Angle of motion', 'Odometer (KM)', 'Internal battery Level (Volts)', 'Signal Strength',
+                   'Mobile country code', 'Mobile network code', 'Cell id', 'Location area code',
+                   '#Ignition(0/1), RESERVED ,Harsh Braking / Acceleration//Non(0/2/3),Main power status(0/1)',
+                   'Over speeding', 'Signature', 'CHECKSUM']
+
+        # Result dictionary
+        result = {}
+
+        # --------- Data Processing ---------
+        for index, header in enumerate(HEADERS):
+            result[header] = gps_data[index]
+
+        return result
+
+
+def convert_OBD_data(obd_data):
+    """
+    Function that'll convert Raw OBD data to Readable JSON Object
+    """
+
+    # --------- Headers for final dictionary ---------
+    HEADERS = ['Live/Memory', 'Signature', 'IMEI', 'Message Type', 'Sequence No', 'Time (GMT)', 'Date', 'OBD Protocol']
+
+    # Result dictionary
+    result = {}
+
+    # --------- Data processing ---------
+    first_half_raw = obd_data[:8]
+    second_half_raw = obd_data[8:-1]
+
+    # --------- First Half Data Processing ---------
+    for index, header in enumerate(HEADERS):
+        result[header] = first_half_raw[index]
+
+    # --------- Second Half Data Processing ---------
+    for pid in second_half_raw:
+        i = pid.split(':')
+        if len(i) > 1:
+            result[i[0]] = i[1]
+
+    return result
+
+
+def convert_raw_to_information(input_data):
+    """
+    Function that'll convert Raw input from OBD to formatted dictionary containing all the information
+    needed for the UI
+    """
+    # --------- Data decoding from byte to str ---------
+    input_file = input_data.decode("UTF-8", errors='ignore')
+    # print("[UTF-8 Converted Data] \n {}".format(input_file))
+
+    # --------- Data splitting based on comma ---------
+    input_file = input_file.replace(';', ',')
+    raw_data = input_file.split(',')
+    # print("[UTF-8 Raw Data List] \n {}".format(raw_data))
+
+    # --------- Check for Login packet ---------
+    if len(raw_data) < 8:
+        print("[LOGIN PACKET]: ", raw_data)
+        login_data = convert_LOGIN_data(raw_data)
+        return login_data
+
+    # --------- GPS vs OBD Data ---------
+    elif raw_data[1] == "ATL":
+        print("[GPS PACKET]: ", raw_data)
+        gps_data = convert_GPS_data(raw_data)
+        return gps_data
+
+    elif raw_data[1] == "ATLOBD":
+        print("[OBD PACKET]: ", raw_data)
+        obd_data = convert_OBD_data(raw_data)
+        return obd_data
+    # -----------------------------------
+
+
+if __name__ == '__main__':
+
+    HOST = '192.168.29.30'  # Standard loopback interface address (localhost)
+    PORT = 21212  # Port to listen on (non-privileged ports are > 1023)
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print("Server is Listening...")
+        print("Please Wait")
+        count = 0
+        gpslist_lat=[]
+        gpslist_lon=[]
+        while True:
+            conn, addr = s.accept()
+            print("Conneting..")
+
+            with conn:
+                print('Connected by', addr)
+                while True:
+                    data = conn.recv(10240)
+                    print("TimeStamp: ", datetime.datetime.now())
+                    print(data)
+                    if not data:
+                        break
+                    if convert_raw_to_information(data)["Latitude"]:
+                        lat = convert_raw_to_information(data)["Latitude"]
+                        lon = convert_raw_to_information(data)["Longitude"]
+                        if count == 0:
+                            gpslist_lat.insert(0,lat)
+                            gpslist_lon.insert(0,lon)
+                            gps_one(lat,lon)
+                        else:
+                            gps_main(gpslist_lat[0],gpslist_lon[0],lat,lon)
+                        count += 1
+                        print("initial:",gpslist_lat[0],gpslist_lon[0])
+                        print("live: ",lat,lon)
+
+                    a = b'@866039048589957,00,1234,*CS'
+                    conn.send(b'@866039048589171,00,0518,*CS')
+                    print("--------------------------------------------------------------------------------------------")
