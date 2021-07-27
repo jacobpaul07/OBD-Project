ef52a4216b719b372c6ca55707d34f09bc00ca54
Jacob Paul <46052000+jacobpaul07@users.noreply.github.com>
2021-07-27 11:40:07 +0530

    Update obd_s3_gps_formatted.py

--- GPS-OBD2/obd_s3_gps_formatted.py
+++ GPS-OBD2/obd_s3_gps_formatted.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
             with conn:
                 print('Connected by', addr)
                 while True:
-                    data = conn.recv(1024)
+                    data = conn.recv(4096)
                     print("TimeStamp: ", datetime.datetime.now())
                     print(data)
                     if not data:
